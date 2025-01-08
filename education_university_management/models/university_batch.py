d2e224a8ae96436d128b08cf5ed929f94a2c1a82
Amr Gebil <amrgebil1@gmail.com>
2025-01-08 14:06:29 +0200

    i have added sending email action

--- education_university_management/models/university_batch.py
+++ education_university_management/models/university_batch.py
@@ -19,7 +19,8 @@
 #    If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-from odoo import api, fields, models
+from odoo import fields, models, api, _
+from base64 import b64encode
 
 
 class UniversityBatch(models.Model):
@@ -65,6 +66,8 @@ class UniversityBatch(models.Model):
                                         string="Students",
                                         help="Students of this Batch")
     year_id = fields.Many2one('university.year', string="Year", required=True)
+    timetable_report = fields.Binary(string="Timetable Report", readonly=True)
+    timetable_report_name = fields.Char(string="Report Filename", readonly=True)
 
 
     def action_open_attendance_wizard(self):
@@ -75,3 +78,64 @@ class UniversityBatch(models.Model):
             'view_mode': 'form',
             'target': 'new',
         }
+
+
+    def action_send_mail(self):
+        """Generate timetable report and send it via email to all students in the batch."""
+        self.ensure_one()
+
+        # Fetch the report action
+        report_service = self.env.ref('education_university_management.report_timetable')
+        if not report_service:
+            raise ValueError("Timetable report action is not defined.")
+
+        # Generate the PDF report
+        pdf_data, x = self.env['ir.actions.report'].sudo()._render_qweb_pdf(
+            'education_university_management.report_timetable', [self.timetable_id.id]
+        )
+        if not pdf_data:
+            raise ValueError("Failed to generate the timetable report.")
+
+        # Encode the PDF data as base64
+        encoded_pdf = b64encode(pdf_data)
+
+        # Set the filename for the PDF
+        file_name = f"{self.name}_Timetable_Report.pdf"
+
+        for student in self.batch_student_ids:
+            if not student.email:
+                continue  # Skip if the student does not have an email
+
+            # Compose the email body
+
+            mail_message = _(
+                "<p>Hello <strong>_person_name_</strong>,</p>"
+                "<p>Please find your timetable attached to this email.</p>"
+                "<p>Thanks,</p>"
+                "<p>The University Team</p>"
+            )
+            mail_message = mail_message.replace("_person_name_", student.name or "Student")
+
+            # mail_message = _(
+            #     "<p>Hello <strong>{name}</strong>,</p>"
+            #     "<p>Please find your timetable attached to this email.</p>"
+            #     "<p>Thanks,</p>"
+            #     "<p>The University Team</p>"
+            # ).format(name=student.name or "Student")
+
+            # Prepare email values
+            values = {
+                'subject': f"Timetable for {self.name}",
+                'body_html': mail_message,
+                'email_from': self.email_sender_id.smtp_user,
+                'email_to': student.email,
+                'attachment_ids': [(0, 0, {
+                    'name': file_name,
+                    'type': 'binary',
+                    'datas': encoded_pdf,
+                    'mimetype': 'application/pdf',
+                })],
+            }
+
+            # Create and send the email
+            self.env['mail.mail'].sudo().create(values).send()
