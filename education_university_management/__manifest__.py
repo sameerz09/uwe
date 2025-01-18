cd72d18fba8dae8b27427bc005655a6115fd148a
sameerz09 <sameerz09@hotmail.com>
2025-01-18 14:58:16 +0200

    fixing bugs4

--- education_university_management/__manifest__.py
+++ education_university_management/__manifest__.py
@@ -82,7 +82,7 @@
         'views/attendance_report_wizard_views.xml',
         'views/university_exam_result_views.xml',
         'views/student_fees_views.xml',
-        'views/portal_helpdesk_ticket.xml',
+        # 'views/portal_helpdesk_ticket.xml',
         'report/ir_actions_report.xml',
         'report/ir_actions_report_templates.xml',
         'report/timetable_schedule_reports.xml',
