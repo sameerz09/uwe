e991d2e9ee6719ff8b362c64c576340cfa28e470
sameerz09 <sameerz09@hotmail.com>
2025-09-27 14:43:56 +0300

    Revert "Last edit"
    This reverts commit 3b31e508b9ab7f9deb9aca4c0e9e229541287499.

---
+++ uwe_payroll_list_view/README.rst
@@ -0,0 +1,27 @@
+UWE Payroll List View
+====================
+
+This module customizes the payroll list view for University of Wisconsin.
+
+Features
+--------
+
+* Enhanced list view with Department and Net Wage columns
+* UWE-specific customizations
+
+Installation
+------------
+
+1. Copy this module to your Odoo addons directory
+2. Update the module list
+3. Install the module
+
+Author
+------
+
+University of Wisconsin Development Team
+
+License
+-------
+
+AGPL-3
---
+++ uwe_payroll_list_view/models/hr_payslip.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+class HrPayslip(models.Model):
+    _inherit = 'hr.payslip'
+
+    employee_department = fields.Char(
+        string='Department',
+        related='employee_id.department_id.name',
+        store=True,
+        readonly=True
+    )
+    
+    net_wage_display = fields.Float(
+        string='Net Wage',
+        compute='_compute_net_wage_display',
+        store=True
+    )
+
+    @api.depends('line_ids', 'line_ids.total')
+    def _compute_net_wage_display(self):
+        for payslip in self:
+            net_line = payslip.line_ids.filtered(lambda l: l.code == 'NET')
+            payslip.net_wage_display = net_line.total if net_line else 0.0
